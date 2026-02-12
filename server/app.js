98221397a017cb5551131a5266b66653cb9f61f3
Selehadin Nasir <nasir.selehadin@gmail.com>
2026-02-12 10:26:26 +0300

    feat: added env variable supper and project config

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
@@ -8,4 +9,6 @@ app.use(express.json());
 
 app.get('/', (request, response) => response.json({retruned: "nothing, we are just in the deve;opemtn phase"}))
 
+console.log(process.env);
+console.log("the uri", process.env.MONGODB_URI);
 module.exports = app
