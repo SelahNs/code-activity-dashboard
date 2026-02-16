36611066303ba01a3e103a820da5b73b6726803e
Selehadin Nasir <nasir.selehadin@gmail.com>
2026-02-16 21:10:02 +0300

    feat: added key sto and chars couting

--- extension/extension.js
+++ extension/extension.js
@@ -1,28 +1,50 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const currentSession = {
+	keystrokes: 0,
+	linesAdded: 0,
+	charsDeleted: 0,
+	linesDeleted: 0,
+	charsAdded: 0,
+}
+
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "extension" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
+	
 	const disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
+		
 		vscode.window.showInformationMessage('Hello World from extension!');
 	});
+
+	const disposable2 = vscode.workspace.onDidChangeTextDocument((event) => {
+		event.contentChanges.forEach(change => {
+
+			const isSingleChar = change.text.length === 1;
+			const isEnter = change.text === '\n' || change.text === '\r\n'
+			const isDelete = change.rangeLength > 0 && change.text.length === 0;
+
+			if (isSingleChar || isDelete || isEnter) {
+				currentSession.keystrokes += 1;
+			} 
+			if (change.text.length > 1 && !isEnter){
+				currentSession.charsAdded += change.text.length
+			}
+				currentSession.linesAdded += change.text.split("\n").length - 1
+			
+				currentSession.charsDeleted += change.rangeLength;	
+
+			currentSession.linesDeleted += change.range.end.line - change.range.start.line;
+			
+
+		})
+	})
+
+	context.subscriptions.push(disposable2);
+
 
 	context.subscriptions.push(disposable);
 }
